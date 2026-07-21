f116f733e3e2dda6da01d30344bfc18e658a072b
teruya7-art <teruya7@view.ocn.ne.jp>
2026-07-21 20:10:23 +0900

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// =========================
+// ハンバーガーメニュー
+// =========================
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+if (menuToggle && navMenu) {
+    menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("show");
+    });
+}
